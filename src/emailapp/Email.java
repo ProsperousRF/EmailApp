2ef8772f43b9f368fa62f9a8f4f16e90b692fce6
Stanislav Rakitov <prosperousrf@gmail.com>
2021-11-02 13:28:03 +0300

    Email todo's

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,8 +1,6 @@
 package emailapp;
 
-/**
- * @author Stanislav Rakitov
- */
+/** @author Stanislav Rakitov */
 public class Email {
   private String firstName;
   private String lastName;
@@ -11,5 +9,16 @@ public class Email {
   private int mailboxCapacity;
   private String alternateEmail;
 
+  // TODO: 02.11.2021 Constructor to receive the first name and the last name
+
+  // TODO: 02.11.2021 Ask for the department
+
+  // TODO: 02.11.2021 Generate a random password
+
+  // TODO: 02.11.2021 Set the mailbox capacity
+
+  // TODO: 02.11.2021 Set the alternate email
+
+  // TODO: 02.11.2021 Change the password
 
 }
